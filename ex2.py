9f0e2d7b09e3504c7e93fe96b44160634f83434c
Jooress <130586638+Jooress@users.noreply.github.com>
2023-04-12 11:38:08 -0600

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A Coment, this is so you can read your program later
+# Anything after the # is ignored by phython
+
+print("I could have a code like this.") #and the comment after is ignored
+
+# Ypu can also use a comment to "disable" or comment our code:
+# print("This won't run.")
+
+print("This will run.")
